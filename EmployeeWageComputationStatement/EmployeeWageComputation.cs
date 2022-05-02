d6af9a131be7c1118249ef98172d1fbf16908b79
fatmazehra341 <fatmazehra341@gmail.com>
2022-05-02 14:08:43 +0530

    [Fatma] add :Dislaying employee Attendence

---
+++ EmployeeWageComputationStatement/EmployeeWageComputation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationStatement
+{
+    internal class EmployeeWageComputation
+    {
+        const int IS_PRESENT=1;
+        const int IS_ABSENT=2;
+        public void DisplyayAttendence()
+        {
+        Random random = new Random();
+            int check = random.Next(1, 3);
+            switch(check)
+            {
+                case IS_PRESENT:
+                    Console.WriteLine("Employee is present");
+                    break;
+                    case IS_ABSENT:
+                    Console.WriteLine("Employee is absent");
+                    break ;
+            }
+        }
+    }
+}
---
+++ EmployeeWageComputationStatement/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using EmployeeWageComputationStatement;
+class Program
+{
+    public static void Main( string [] args)
+    {
+        EmployeeWageComputation emp = new EmployeeWageComputation ();
+        emp.DisplyayAttendence();
+    }
+}
